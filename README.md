43fedeb132c8fcf15941fce455b99ab1d5645531
anlena <superiron_man@163.com>
2018-01-18 18:29:19 +0800

    react-music

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-music
 用react实现音乐app
+
+
+
+[学习](https://juejin.im/post/5a367e996fb9a0450f22050f)
+
---
+++ music/src/components/ranking/Ranking.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Ranking.styl"
+
+
+class Ranking extends React.Component {
+    render() {
+        return (
+            <div className="music-ranking">
+                Ranking
+            </div>
+        );
+    }
+}
+
+export default Ranking
